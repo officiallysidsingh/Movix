fa2c614ab910ee8d443fca1acabbc2f9be1633c0
sidsinghrajput <siddharthsingh2014@gmail.com>
2023-03-02 01:15:56 +0530

    added carousel component

---
+++ front-end/src/components/carousel/Carousel.jsx
@@ -0,0 +1,23 @@
+import React, { useRef } from "react";
+import {
+    BsFillArrowLeftCircleFill,
+    BsFillArrowRightCircleFill,
+} from "react-icons/bs";
+
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import dayjs from "dayjs";
+
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+import Img from "../lazyLoadImage/Img";
+import PosterFallback from "../../assets/no-poster.png";
+
+import "./style.scss";
+
+const Carousel = () => {
+  return (
+    <div className=''>Carousel</div>
+  );
+};
+
+export default Carousel;
