251d7d2f29d91f7a9d5acef0f9cb0f3f143cb942
sidsinghrajput <siddharthsingh2014@gmail.com>
2023-03-02 01:14:46 +0530

    added styles of tabSwitch to Global Home Page

--- front-end/src/pages/home/Home.jsx
+++ front-end/src/pages/home/Home.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 import './style.scss';
 
 import HeroBanner from './heroBanner/HeroBanner';
+import Trending from './trending/Trending';
 
 const Home = () => {
   return (
     <div className='homePage'>
-      <HeroBanner/>
+      <HeroBanner />
+      <Trending />
       <div style={{ height: 1000 }}></div>
     </div>
   )
