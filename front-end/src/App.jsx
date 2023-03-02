1ea717e53be78076724d3c8b05634c32e226e60b
sidsinghrajput <siddharthsingh2014@gmail.com>
2023-03-02 22:53:54 +0530

    added popular, trending and top-rated section using reusable carousel component made

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 import { fetchDataFromApi } from "./utils/api";
 
 import { useSelector, useDispatch } from 'react-redux';
-import { getApiConfiguration } from './store/homeSlice';
+import { getApiConfiguration, getGenres } from './store/homeSlice';
 
 import AppRouter from './AppRouter';
 
@@ -14,7 +14,8 @@ function App() {
   
   useEffect(() => {
     fetchApiConfig();
-  }, [])
+    genresCall();
+  }, []);
 
   const fetchApiConfig = () => {
     fetchDataFromApi('/configuration')
@@ -28,8 +29,25 @@ function App() {
         }
 
         dispatch(getApiConfiguration(url));
-      })
-  }
+      });
+  };
+
+  const genresCall = async () => {
+    let promises = [];
+    let endPoints = ["tv", "movie"];
+    let allGenres = {};
+
+    endPoints.forEach((url) => {
+      promises.push(fetchDataFromApi(`/genre/${url}/list`))
+    });
+    
+    const data = await Promise.all(promises);
+    data?.map(({ genres }) => {
+      return genres?.map((item) => (allGenres[item.id] = item))
+    });
+
+    dispatch(getGenres(allGenres));
+  };
 
   return (
       <AppRouter />
