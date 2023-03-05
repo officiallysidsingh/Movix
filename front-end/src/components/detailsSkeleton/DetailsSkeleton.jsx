ff8483e4af74b7e90651d5155daf001ca1f6d9db
sidsinghrajput <siddharthsingh2014@gmail.com>
2023-03-05 18:03:50 +0530

    completed the whole details section and its skeleton for fallback

--- front-end/src/components/detailsSkeleton/DetailsSkeleton.jsx
+++ front-end/src/components/detailsSkeleton/DetailsSkeleton.jsx
@@ -5,23 +5,82 @@ import "./style.scss";
 import ContentWrapper from "../contentWrapper/ContentWrapper";
 
 const DetailsSkeleton = () => {
-  return (
-    <div className="detailsBanner">
-      <div className="detailsBannerSkeleton">
-        <ContentWrapper>
-          <div className="left skeleton"></div>
-          <div className="right">
-            <div className="row skeleton"></div>
-            <div className="row skeleton"></div>
-            <div className="row skeleton"></div>
-            <div className="row skeleton"></div>
-            <div className="row skeleton"></div>
-            <div className="row skeleton"></div>
-            <div className="row skeleton"></div>
-          </div>
-        </ContentWrapper>
+  const skeleton = () => {
+    return (
+      <div className="skItem">
+        <div className="circle skeleton"></div>
+        <div className="row skeleton"></div>
+        <div className="row2 skeleton"></div>
       </div>
-    </div>
+    );
+  };
+
+  const loadingSkeleton = () => {
+    return (
+      <div className="skItem">
+        <div className="thumb skeleton"></div>
+        <div className="row skeleton"></div>
+        <div className="row2 skeleton"></div>
+      </div>
+    );
+  };
+
+  const skItem = () => {
+    return (
+      <div className="skeletonItem">
+        <div className="posterBlock skeleton">
+          <div className="textBlock">
+            <div className="title skeleton"></div>
+            <div className="date skeleton"></div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <ContentWrapper>
+      <div className="detailsBannerSkeleton">
+        <div className="left skeleton"></div>
+        <div className="right">
+          <div className="row skeleton"></div>
+          <div className="row skeleton"></div>
+          <div className="row skeleton"></div>
+          <div className="row skeleton"></div>
+          <div className="row skeleton"></div>
+          <div className="row skeleton"></div>
+          <div className="row skeleton"></div>
+        </div>
+      </div>
+      <div className="castSkeleton">
+        {skeleton()}
+        {skeleton()}
+        {skeleton()}
+        {skeleton()}
+        {skeleton()}
+        {skeleton()}
+      </div>
+      <div className="videoSkeleton">
+        {loadingSkeleton()}
+        {loadingSkeleton()}
+        {loadingSkeleton()}
+        {loadingSkeleton()}
+      </div>
+      <div className="loadingSkeleton">
+        {skItem()}
+        {skItem()}
+        {skItem()}
+        {skItem()}
+        {skItem()}
+      </div>
+      <div className="loadingSkeleton">
+        {skItem()}
+        {skItem()}
+        {skItem()}
+        {skItem()}
+        {skItem()}
+      </div>
+    </ContentWrapper>
   );
 };
 
