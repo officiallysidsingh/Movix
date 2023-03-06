599704299bd666a2279df3a01f431f962d3eecb7
sidsinghrajput <siddharthsingh2014@gmail.com>
2023-03-06 17:13:49 +0530

    added search functionality

--- front-end/src/components/header/Header.jsx
+++ front-end/src/components/header/Header.jsx
@@ -10,101 +10,111 @@ import ContentWrapper from "../contentWrapper/ContentWrapper";
 import logo from "../../assets/movix-logo.svg";
 
 const Header = () => {
-    const [show, setShow] = useState("top");
-    const [lastScrollY, setLastScrollY] = useState(0);
-    const [mobileMenu, setMobileMenu] = useState(false);
-    const [query, setQuery] = useState("");
-    const [showSearch, setShowSearch] = useState("");
+  const [show, setShow] = useState("top");
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [query, setQuery] = useState("");
+  const [showSearch, setShowSearch] = useState("");
 
-    const navigate = useNavigate();
-    const location = useLocation();
+  const navigate = useNavigate();
+  const location = useLocation();
 
-    useEffect(() => {
-      window.scrollTo(0, 0);
-    }, [location])
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
 
-    const controlNavbar = () => {
-      if(window.scrollY > 200) {
-        if(window.scrollY > lastScrollY && !mobileMenu) {
-          setShow("hide");
-        } else {
-          setShow("show");
-        }
-        setLastScrollY(window.scrollY);
+  const controlNavbar = () => {
+    if (window.scrollY > 200) {
+      if (window.scrollY > lastScrollY && !mobileMenu) {
+        setShow("hide");
       } else {
-        setShow("top");
+        setShow("show");
       }
+      setLastScrollY(window.scrollY);
+    } else {
+      setShow("top");
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", controlNavbar);
+
+    return () => {
+      window.removeEventListener("scroll", controlNavbar);
     };
+  }, [lastScrollY]);
 
-    useEffect(() => {
-      window.addEventListener("scroll", controlNavbar);
-
-      return () => {
-        window.removeEventListener("scroll", controlNavbar);
-      };
-    }, [lastScrollY]);
-
-    const searchQueryHandler = (event) => {
-      if(event.key === 'Enter' && query.length > 0) {
-        navigate(`/search/${query}`);
-        setTimeout(() => {
-          setShowSearch(false);
-        }, 1000);
-      }
-    };
-
-    const openSearch = () => {
-      setMobileMenu(false);
-      setShowSearch(true);
+  const searchQueryHandler = (event) => {
+    if (event.key === "Enter" && query.length > 0) {
+      navigate(`/search/${query}`);
+      setTimeout(() => {
+        setShowSearch(false);
+      }, 1000);
     }
+  };
 
-    const openMobileMenu =() => {
-      setMobileMenu(true);
-      setShowSearch(false);
-    }
+  const openSearch = () => {
+    setMobileMenu(false);
+    setShowSearch(true);
+  };
 
-    const navigationHandler = (type) => {
-      if(type === "movie") {
-        navigate("/explore/movie");
-      }else {
-        navigate("explore/tv");
-      }
-      setMobileMenu(false);
+  const openMobileMenu = () => {
+    setMobileMenu(true);
+    setShowSearch(false);
+  };
+
+  const navigationHandler = (type) => {
+    if (type === "movie") {
+      navigate("/explore/movie");
+    } else {
+      navigate("explore/tv");
     }
-    return (
-        <header className={`header ${mobileMenu ? "mobileView" : ""} ${show}`}>
+    setMobileMenu(false);
+  };
+  return (
+    <header className={`header ${mobileMenu ? "mobileView" : ""} ${show}`}>
+      <ContentWrapper>
+        <div className="logo" onClick={() => navigate("/")}>
+          <img src={logo} alt="logo" />
+        </div>
+        <ul className="menuItems">
+          <li className="menuItem" onClick={() => navigationHandler("movie")}>
+            Movies
+          </li>
+          <li className="menuItem" onClick={() => navigationHandler("tv")}>
+            TV Shows
+          </li>
+          <li className="menuItem">
+            <HiOutlineSearch onClick={openSearch} />
+          </li>
+        </ul>
+
+        <div className="mobileMenuItems">
+          <HiOutlineSearch onClick={openSearch} />
+          {mobileMenu ? (
+            <VscChromeClose onClick={() => setMobileMenu(false)} />
+          ) : (
+            <SlMenu onClick={openMobileMenu} />
+          )}
+        </div>
+      </ContentWrapper>
+      {showSearch ? (
+        <div className="searchBar">
           <ContentWrapper>
-            <div className="logo">
-              <img src={logo} alt="logo" />
-            </div>
-            <ul className="menuItems">
-              <li className="menuItem" onClick={() => navigationHandler("movie")}>Movies</li>
-              <li className="menuItem" onClick={() => navigationHandler("tv")}>TV Shows</li>
-              <li className="menuItem">
-                <HiOutlineSearch onClick={openSearch} />
-              </li>
-            </ul>
-
-            <div className="mobileMenuItems">
-              <HiOutlineSearch onClick={openSearch} />
-              { mobileMenu ? <VscChromeClose onClick={() => setMobileMenu(false)} /> : <SlMenu onClick={openMobileMenu} /> }
+            <div className="searchInput">
+              <input
+                type="text"
+                placeholder="Search for a movie or tv show..."
+                onChange={(e) => setQuery(e.target.value)}
+                onKeyUp={searchQueryHandler}
+              />
+              <VscChromeClose onClick={() => setShowSearch(false)} />
             </div>
           </ContentWrapper>
-          {showSearch ? (<div className="searchBar">
-            <ContentWrapper>
-              <div className="searchInput">
-                <input 
-                  type="text"
-                  placeholder='Search for a movie or tv show...'
-                  onChange={(e) => setQuery(e.target.value)}
-                  onKeyUp={searchQueryHandler}
-                />
-                <VscChromeClose onClick={() => setShowSearch(false)} />
-              </div>
-            </ContentWrapper>
-          </div>) : null}
-        </header>
-    );
+        </div>
+      ) : null}
+    </header>
+  );
 };
 
 export default Header;
